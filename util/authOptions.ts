42394a9b9234a8a484959c1e88ddf2aef40c513f
Nadav Salomon <nadav.543.345@gmail.com>
2024-06-01 20:32:30 +0300

    authFormFix

--- util/authOptions.ts
+++ util/authOptions.ts
@@ -1,76 +1,64 @@
-// Import bcrypt for password hashing
 import bcrypt from "bcrypt";
-// Import NextAuth and related types for authentication
-import { AuthOptions } from "next-auth";
-// Import credential and social providers for NextAuth
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-// Import PrismaAdapter to integrate Prisma with NextAuth
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// Import the Prisma client instance for database interactions
+
 import prisma from "@/app/libs/prismadb";
 
-// Define the authentication configuration
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma), // Use Prisma adapter for database management
+  adapter: PrismaAdapter(prisma),
   providers: [
-    // Define authentication providers
     GithubProvider({
-      // GitHub OAuth provider configuration
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
-      // Google OAuth provider configuration
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
-      // Custom credentials provider for email and password
-      name: "credentials",
+      name: 'credentials',
       credentials: {
-        // Define the fields required for credentials
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
       },
       async authorize(credentials) {
-        // Function to authenticate the user with email and password
         if (!credentials?.email || !credentials?.password) {
-          // Validate input credentials
-          throw new Error("Invalid Credentials");
+          throw new Error('Invalid Credentials');
         }
+
         const user = await prisma.user.findUnique({
-          // Retrieve the user from the database
           where: {
-            email: credentials.email,
-          },
+            email: credentials.email
+          }
         });
 
-        if (!user || !user.hashedPassword) {
-          // Check if user exists and has a hashed password
-          throw new Error("Invalid Credentials");
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
         }
 
         const isCorrectPassword = await bcrypt.compare(
-          // Verify the password against the hashed password
           credentials.password,
           user.hashedPassword
         );
 
         if (!isCorrectPassword) {
-          // Password does not match
-          throw new Error("Invalid Credentials");
+          throw new Error('Invalid credentials');
         }
 
-        return user; // Return user object if authentication succeeds
-      },
-    }),
+        return user;
+      }
+    })
   ],
-  debug: process.env.NODE_ENV === "development", // Enable debug mode in development
+  debug: process.env.NODE_ENV === 'development',
   session: {
-    // Configure session management
-    strategy: "jwt", // Use JWT for session strategy
+    strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET, // Secret for NextAuth to sign tokens
+  secret: process.env.NEXTAUTH_SECRET,
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
